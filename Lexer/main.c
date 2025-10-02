5beb4790224bbd9fb69693b1d009114a902763fc
Michal Ukropec <michalukropec@Michals-MacBook-Pro.local>
2025-10-02 22:39:17 +0200

    TODO: Allocate in / out and do trasformation.

--- Lexer/main.c
+++ Lexer/main.c
@@ -5,11 +5,47 @@
 
 #define ARENA_SIZE 1000
 
+enum TokenKind {
+    TK_U32 = 0, 
+    TK_LPAREN = 1, TK_RPAREN = 2,
+    TK_LSQLR = 3, TK_RSQRL = 4,
+    TK_SEMICOL = 5,
+    TK_IDENT = 6
+};
+
 struct Arena {
     long size;
     long index;
     char* base;
 };
+
+struct Token {
+    enum TokenKind kind;
+    char* val;
+};
+
+int 
+is_waste(char ch) {
+    switch(ch) {
+        case '\n': case '\r': case ' ': case '\t': {
+            return 0;
+        } break;
+    }
+
+    return 1;
+}
+
+int
+is_number(char ch) {
+    switch(ch) {
+        case 0: case 1: case 2: case 3: case 4: 
+        case 5: case 6: case 7: case 8: case 9: {
+            return 0;
+        } break;
+    }
+
+    return 1;
+}
 
 int main(void) {
     FILE *fp;
@@ -31,7 +67,20 @@ int main(void) {
         arena->index += 1;
         arena->base[arena->index] = symbol;
     }
+    arena->base[arena->index] = '\0';
 
     fclose(fp);
+
+    iterator = 1;
+    while((symbol = arena->base[iterator++])) {
+        if(is_waste(symbol) == 0)
+            continue;
+
+        if(is_number(symbol) == 0)
+            for()
+
+        printf("Lexed char: %c\n", symbol);
+    }
+
     return 0;
 }
